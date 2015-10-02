ba8fe4dfcf1290c8bf955a21bd82367859ee1502
StephenRUK <stephen_aoe@hotmail.com>
2015-10-02 13:40:22 +0200

    Fixed model data. Added getByID
    Fixed the array of models by adding names, path and file format.
    Added getByID function used for binding to model selection dropdown.

--- js/ModelService.js
+++ js/ModelService.js
@@ -9,19 +9,20 @@
 function ModelServiceStatic() {
 
 	var models = [
-		new Model("Box", "Box/Box.obj", ModelFormat.OBJMTL),
-		new Model("Cross Scene", "CrossScene/CrossScene.obj", ModelFormat.OBJMTL),
-		new Model("male02", "Male Three.js example", ModelFormat.OBJMTL),
-		new Model("Spaceship", "Spaceship", ModelFormat.OBJMTL),
-		new Model("StephenHouse", "House Architecture"),
-		new Model("KARSYS_1/KARSYS_1.dae", ModelFormat.DAE)
+		new Model("Box", "models/Box/Box.obj", ModelFormat.OBJMTL),
+		new Model("Cross Scene", "models/CrossScene/CrossScene.obj", ModelFormat.OBJMTL),
+		new Model("Male Three.js example", "models/male02/male02.obj", ModelFormat.OBJMTL),
+		new Model("Spaceship", "models/Spaceship/Spaceship.obj", ModelFormat.OBJMTL),
+		new Model("House", "models/StephenHouse/StephenHouse.obj", ModelFormat.OBJMTL),
+		new Model("KARSYS Model 1", "models/KARSYS_1/KARSYS_1.dae", ModelFormat.DAE)
 	];
 
 	this.getAll = function () {
 		return models;
 	};
-	
-	
-
+    
+    this.getByID = function(id) {
+        return models[id];
+    };
 
 }
