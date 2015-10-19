b25aec350a00b108dacef05e05c44a1914680d5a
StephenRUK <stephen_aoe@hotmail.com>
2015-10-19 15:17:11 +0200

    Improved hierarchy objects filter
    Angular filter is used to remove unwanted objects, and don't show
    lowest-level items as expandable in the tree

--- js/app.js
+++ js/app.js
@@ -6,3 +6,19 @@ app.value('canvasID', 'canvas');
 app.service('GraphicsSvc', ['canvasID', GraphicsService]);
 
 app.controller('ModelController', ['ModelRepo', 'GraphicsSvc', '$scope', ModelController]);
+
+app.filter('listObjectInTree', function () {
+  return function (sceneObjects) {
+    var filtered = [];
+      
+    for (var i = 0; i < sceneObjects.length; i++) {
+      var item = sceneObjects[i];
+        
+      if (item.name.length > 0) {
+        filtered.push(item);
+      }
+    }
+      
+    return filtered;
+  };
+});
