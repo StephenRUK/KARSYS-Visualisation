1bdc3379f58c8a6d5ac935f9117e539230db57e3
StephenRUK <stephen_aoe@hotmail.com>
2015-11-24 12:57:45 +0100

    Working query on DB to JSON
    Multiple types of data can be retrieved and are output as JSON strings

--- backend/gisapi.php
+++ backend/gisapi.php
@@ -1,41 +1,61 @@
 <?php
-include('db.php');  // TODO improve security
+require('db.php');  // TODO improve security
 
 /************************************************
 * Top-level methods
 ************************************************/
 
 function getTypeIdFromObjectId ($objectID) {
-    return $objectID.substring(2);
+    return substr($objectID, 0, 2);
 }
 
-function getObjectData ($objectID) {
+function getObjectData ($dbConn, $objectID) {
     $typeID = getTypeIdFromObjectId ($objectID);
-    $data = null;
+    $query = "";
     
     switch ($typeID) {
-        case 14:
-            $data = getDataSprings($objectID);
+        case 12:    // Cross-section
+            $query = "SELECT cs_filenam AS 'File Name', cs_author AS 'Author', cs_rem AS 'Comments' FROM cs WHERE cs_id = '" . $objectID . "'";
             break;
-        case 24:
-            $data = getDataPhreaticZone($objectID);
+        case 17:    // po???
+            $query = "SELECT po_name AS 'Name', po_x AS 'x', po_y AS 'y', po_z AS 'z', po_rem AS 'Comments' FROM po WHERE po_id = '" . $objectID . "'";
             break;
+        case 18:    // tr???
+            $query = "SELECT tr_po_id AS 'Related PO ID', tr_rem AS 'Comments' FROM tr WHERE tr_id = '" . $objectID . "'";
+            break;
+        case 14:    // Springs
+            $query = "SELECT ip_name AS 'Name', ip_rem AS 'Comments' FROM ip WHERE ip_id = '" . $objectID . "'";
+            break;
+        case 24:    // Phreatic Zone
+            $query = "SELECT nk_type AS 'Type', nk_vol AS 'Volume' FROM nk WHERE nk_id = '" . $objectID . "'";
+            break;
+        case 52:    // oq???
+            $query = "SELECT oq_type AS 'Type', oq_rem AS 'Comments' FROM oq WHERE oq_id = '" . $objectID . "'";
+            break;
+            
     }
     
-    return $data;
+    if ($query) {
+        return doQueryAssoc($dbConn, $query);
+    } else {
+        return array(); // Return empty array representing "no data"
+    }
+    
 }
 
-function getObjectDataJSON ($objectID) {
-    return json_encode(getObjectData($objectID));
+function getObjectDataJSON ($dbConn, $objectID) {
+    return json_encode(getObjectData($dbConn, $objectID), JSON_PRETTY_PRINT);
 }
 
 /************************************************
 * Methods for retrieving specific type data
 ************************************************/
 
-function getDataSprings($objectID) {
-    $sql = 'SELECT ip_name, ip_rem FROM ip WHERE ip_id = ' . $objectID;
-    $result = $conn->query($sql)->fetch_assoc();
+function doQueryAssoc($dbConn, $sql) {
+    $result = $dbConn->query($sql);
+    if ($result) {
+        $result = $result->fetch_assoc();
+    }
 
     return $result;
 }
@@ -50,13 +70,11 @@ if ($conn->connect_error) {
     die ('SQL Connection failed: ' . $conn->connect_error);
 }
 
-echo "Connected to DB.";
-
-if ($_GET['id']) {    
+if (isset($_GET['id'])) {
     $id = $_GET['id'];   // TODO Maybe need to sanitize input
-    echo 'Object ID: ' . $id;
-    echo getObjectDataJSON($id);
     
+    echo getObjectDataJSON($conn, $id);
+
 }
 
 $conn->close();
