b9807b607f3915462aa271fc09f302bbfb4715f1
StephenRUK <stephen_aoe@hotmail.com>
2015-11-24 13:59:01 +0100

    1st draft of service to get object info
    Validation of specified ID, XHR for retrieving info as JSON

---
+++ js/ArcGisService.js
@@ -0,0 +1,55 @@
+'use strict';
+
+/*********************
+* ArcGisService
+*   Retrieves data for a specific object from the ArcGIS database.
+*   Data is exposed by a REST-like API.
+*********************/
+function ArcGisService(serverURL, $http) {
+    var url,
+        validIdPattern = /^\d{2}(\d{4})?\w{2}\d{4}$/;  // Examples: 121203VD0002, 54BS0123
+    
+    /**********************
+    * Private API
+    **********************/
+    
+    function performQuery(queryString) {
+        return $http
+            .get(url + '?' + queryString)
+            .then(function (result) {
+                return result.data;
+            });
+    }
+    
+    function init() {
+        // Test server connectivity
+        url = serverURL;
+    }
+    
+    /**********************
+    * Utility methods
+    **********************/
+    
+    function parseID(id) {
+        var idClean = id.trim().replace('_', '');
+        return idClean;
+    }
+    
+    /**********************
+    * Public API
+    **********************/
+    
+    this.isValidID = function (ObjectID) {
+        var idClean = parseID(ObjectID);
+        return validIdPattern.test(idClean);
+    };
+    
+    this.getObjectData = function (ObjectID) {
+        var queryParams = 'id=' + ObjectID;
+        return performQuery(url + queryParams);
+    };
+    
+    /**********************/
+    init();
+    
+}
