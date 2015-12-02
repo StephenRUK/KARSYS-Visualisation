85cc2bc61f943efa07b5ad7850aa46e449678f35
StephenRUK <stephen_aoe@hotmail.com>
2015-12-02 11:31:50 +0100

    Capitalization doesn't change existing capitals

--- js/app.js
+++ js/app.js
@@ -33,6 +33,6 @@ app.filter('listObjectInTree', function () {
 });
 app.filter('capitalize', function() {
     return function(input) {
-      return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1).toLowerCase() : '';
+      return (!!input) ? input.charAt(0).toUpperCase() + input.substr(1) : '';
     }
 });
