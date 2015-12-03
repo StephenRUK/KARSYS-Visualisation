dffd347d60e38da94257a4499d7e0bef96f81984
StephenRUK <stephen_aoe@hotmail.com>
2015-12-03 17:20:58 +0100

    Complete rework to dynamically retrieve fields for object
    Object's fields are retrieved from DB, as well as their display names
    and values. TODO: Retrieve domain values

--- backend/gisapi.php
+++ backend/gisapi.php
@@ -10,33 +10,100 @@ function getTypeIdFromObjectId ($objectID) {
 }
 
 function getTypeIdDetails ($dbConn, $typeId) {
-    $query = "SELECT code,displayname FROM " . DB_Table_DataTypes . " WHERE ID = " . $typeId;
+    $query = "SELECT id, code, displayname FROM " . DB_Table_Data_Types . " WHERE ID = " . $typeId;
     $result = doQueryAssoc($dbConn, $query);
 
     return $result;
 }
 
+function getFieldsForTypeId ($dbConn, $typeID) {
+    $query = "SELECT Field FROM " . DB_Table_Data_Fields . " WHERE LayerID = " . $typeID;
+    $result = $dbConn->query($query);
+    
+    $fields = array();
+    
+    while ($row = $result->fetch_array()) {
+        $fields[] = $row[0];
+    }
+
+    return $fields;
+}
+
+function getFieldDisplayNames ($dbConn, $typeID) {
+    // TODO Add language check in future
+    $query = "SELECT DisplayName FROM " . DB_Table_Data_Fields . " WHERE LayerID = " . $typeID;
+    $result = $dbConn->query($query);
+    
+    $names = array();
+    
+    while ($row = $result->fetch_array()) {
+        $names[] = $row[0];
+    }
+
+    return $names;
+}
+
+function getValuesForFields ($dbConn, $table, $fields, $objectID) {
+    $fieldList = '';
+    foreach ($fields as $field) {
+        $fieldList .= $field . ',';
+    }
+    $fieldList = substr($fieldList, 0, strlen($fieldList)-1);  // Trim last comma
+
+    $query = 'SELECT ' . $fieldList . ' FROM ' . $table . ' WHERE ' . $table . "_id = '" . $objectID . "'";
+    $result = $dbConn->query($query);
+    
+    $values = array();
+    $row = $result->fetch_array(MYSQLI_ASSOC);
+    
+    foreach ($row as $k=>$v) {
+        /*if (is_string($v)) {
+            $v = utf8_encode($v);
+        }*/
+        $values[] = $v;
+    }
+    
+    return $values;
+}
+
 function getObjectData ($dbConn, $objectID) {
     $typeID = getTypeIdFromObjectId($objectID);
     $typeDetails = getTypeIdDetails($dbConn, $typeID);
-    $dataTable = DB_View_Prefix . $typeDetails['code'];
+    
+    if (!$typeDetails) return null;
+    
+    $typeTable = $typeDetails['code'];
+    
+    $fields = getFieldsForTypeId($dbConn, $typeID);                         // Internal field names
+    $fieldDisplayNames = getFieldDisplayNames($dbConn, $typeID);            // Display name for each field
+    $values = getValuesForFields($dbConn, $typeTable, $fields, $objectID);  // Value of each field
+        
+    // Generate output array with title and fields
+    $map = array();
+    $map['title'] = $typeDetails['displayname'];
+    
+    for ($i=0; $i < count($fields); $i++) {
+        // TODO Check if there's a domain value for each field
 
-    $query = "SELECT * FROM " . $dataTable . " WHERE ID = '" . $objectID . "'";
-    $objectData = doQueryAssoc($dbConn, $query);
-    
-    if (!$objectData) {
-        $objectData = array();
+        $map['fields'][] = array(
+            'name' => $fieldDisplayNames[$i],
+            'value' => $values[$i]
+            // TODO get unit and format
+        );
     }
     
-    if ($typeDetails) {
-        $objectData['title'] = $typeDetails['displayname'];
-    }
-    
-    return $objectData;
+    return $map;
 }
 
 function getObjectDataJSON ($dbConn, $objectID) {
-    return json_encode(getObjectData($dbConn, $objectID));
+    $dataArray = getObjectData($dbConn, $objectID);
+    $json = json_encode($dataArray, JSON_UNESCAPED_UNICODE);
+    
+    if (json_last_error()==JSON_ERROR_UTF8) {
+        die('JSON Encode error: Malformed UTF8 characters');
+    }
+    
+    return $json;
 }
 
 /************************************************
@@ -57,6 +124,7 @@ function doQueryAssoc($dbConn, $sql) {
 ************************************************/
 
 $conn = new mysqli(DB_Server, DB_User, DB_Pass, DB_Name);
+$conn->set_charset("utf8"); // IMPORTANT for JSON encoding to work with special characters
 
 if ($conn->connect_error) {
     die ('SQL Connection failed: ' . $conn->connect_error);
